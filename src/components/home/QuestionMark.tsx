5c10cccb8fd042370a5b85efe2680b82bd2c3924
Jedrzej Ratajczak <jratajczak@splashbrands.com>
2024-11-13 22:40:29 +0100

    Remove space

--- src/components/home/QuestionMark.tsx
+++ src/components/home/QuestionMark.tsx
@@ -9,7 +9,7 @@ export function QuestionMark({
 }: QuestionMarkProps) {
   return (
     <span
-      className={`absolute font-poetsen-one text-transparent md:scale-125 xl:scale-150  ${className}`}
+      className={`absolute font-poetsen-one text-transparent md:scale-125 xl:scale-150 ${className}`}
       style={{
         WebkitTextStroke: big ? '3px #FFFFFF' : '2px #FFFFFF'
       }}
