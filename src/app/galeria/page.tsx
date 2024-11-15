f93260e5fc5507ea842fe22c7b23428e580c3bc6
Jedrzej Ratajczak <jratajczak@splashbrands.com>
2024-11-15 16:53:34 +0100

    Add gallery

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,34 +1,41 @@
+import Image from 'next/image';
+
 import { SectionContainer } from '@/components/home';
 import { QuestionFormSection, Title } from '@/home';
 
-function GallerySection() {
-  const fotoConfig = [
-    'row-span-2',
-    '',
-    '',
-    'col-span-2',
-    '',
-    '',
-    'col-span-2 row-span-2',
-    '',
-    'row-span-2',
-    '',
-    'col-span-2',
-    '',
-    '',
-    'col-span-2 row-span-2'
-  ];
+const fotoConfig = [
+  'row-span-2',
+  'aspect-[120/90]',
+  'aspect-[120/90]',
+  'col-span-2 aspect-[260/100]',
+  'aspect-[120/90]',
+  'aspect-[120/90]',
+  'col-span-2 row-span-2 aspect-[120/90]',
+  'aspect-[120/90]',
+  'row-span-2',
+  'aspect-[120/90]',
+  'col-span-2 aspect-[260/100]',
+  'aspect-[120/90]',
+  'aspect-[120/90]',
+  'col-span-2 row-span-2 aspect-[120/90]'
+];
 
-  const photos = [''];
-
-  return (
-    <div className="grid grid-cols-2 gap-4">
-      {photos.map(photo => (
-        <div className={`${fotoConfig}`} key={photo} />
-      ))}
-    </div>
-  );
-}
+const photos = [
+  '/home/gallery/gallery1.webp',
+  '/home/gallery/gallery2.webp',
+  '/home/gallery/gallery3.webp',
+  '/home/gallery/gallery4.webp',
+  '/home/gallery/gallery5.webp',
+  '/home/gallery/gallery6.webp',
+  '/home/gallery/gallery7.webp',
+  '/home/gallery/gallery8.webp',
+  '/home/gallery/gallery9.webp',
+  '/home/gallery/gallery10.webp',
+  '/home/gallery/gallery11.webp',
+  '/home/gallery/gallery12.webp',
+  '/home/gallery/gallery13.webp',
+  '/home/gallery/gallery14.webp'
+];
 
 export default function Gallery() {
   return (
@@ -40,12 +47,25 @@ export default function Gallery() {
           życia Akademii Skateboardingu! Znajdziesz tu ujęcia z naszych zajęć,
           warsztatów, wyjazdów oraz zawodów.
         </p>
-        <p>
+        <p className="mb-10">
           Zobacz, jak nasi uczniowie rozwijają swoje umiejętności, zdobywają
           nowe triki i świetnie się bawią w gronie rówieśników. Każde zdjęcie to
           mała opowieść o pasji, przygodzie i radości z jazdy na deskorolce!
         </p>
-        <GallerySection />
+        <div className="grid grid-cols-2 gap-3 sm:gap-5">
+          {photos.map((photo, index) => (
+            <div key={photo} className={`relative ${fotoConfig[index]}`}>
+              <Image
+                src={photo}
+                alt=""
+                className="pointer-events-none"
+                fill
+                sizes="100vw"
+                style={{ objectFit: 'cover' }}
+              />
+            </div>
+          ))}
+        </div>
       </SectionContainer>
       <QuestionFormSection />
     </>
