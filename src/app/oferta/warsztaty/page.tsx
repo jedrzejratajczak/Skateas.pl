1253eda7bac0915308ad31142a9eda836a5c1e8e
Jedrzej Ratajczak <jratajczak@splashbrands.com>
2024-11-17 00:46:49 +0100

    Add empty pages

--- src/app/oferta/warsztaty/page.tsx
+++ src/app/oferta/warsztaty/page.tsx
@@ -1,3 +1,23 @@
-export default function Warkshops() {
-  return null;
+import {
+  QuestionFormSection,
+  SectionContainer,
+  Title
+} from '@/components/home';
+
+export default function Workshops() {
+  return (
+    <>
+      <SectionContainer>
+        <div className="py-40">
+          <Title
+            title="Strona"
+            subtitle="pojawi się wkrótce"
+            titleClassName="text-[#FBD24D] -left-16"
+            subtitleClassName="left-6"
+          />
+        </div>
+      </SectionContainer>
+      <QuestionFormSection />
+    </>
+  );
 }
--- src/app/oferta/wyjazdy/page.tsx
+++ src/app/oferta/wyjazdy/page.tsx
@@ -1,3 +1,23 @@
+import {
+  QuestionFormSection,
+  SectionContainer,
+  Title
+} from '@/components/home';
+
 export default function Trips() {
-  return null;
+  return (
+    <>
+      <SectionContainer>
+        <div className="py-40">
+          <Title
+            title="Strona"
+            subtitle="pojawi się wkrótce"
+            titleClassName="text-[#FBD24D] -left-16"
+            subtitleClassName="left-6"
+          />
+        </div>
+      </SectionContainer>
+      <QuestionFormSection />
+    </>
+  );
 }
