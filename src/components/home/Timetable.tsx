c5648b354d0df094ed4610384324c4e9e4556cb0
Jędrzej Ratajczak <nilphym@gmail.com>
2024-12-02 22:15:52 +0100

    Update groups

--- src/components/home/Timetable.tsx
+++ src/components/home/Timetable.tsx
@@ -14,16 +14,16 @@ export function Timetable() {
             height={16.41}
             className="m-auto sm:mb-2 sm:scale-150 lg:mb-4 lg:scale-[2]"
           />
-          <span>9-15 lat</span>
+          <span>Każdy wiek</span>
         </p>
         <div className="flex flex-col justify-center gap-2">
-          <p>Grupa podstawowa / średnio zaawansowana</p>
-          <p>Śr (czas do ustalenia)</p>
+          <p>Grupa średnio zaawansowana</p>
+          <p>Wtorki 16:30 - 18:00</p>
         </div>
         <SignupModalButton
           color="green"
           type="group"
-          group="9-15 lat, grupa podstawowa / średnio zaawansowana, śr (czas do ustalenia)"
+          group="Grupa średnio zaawansowana, wtorki 16:30 - 18:00"
         >
           Zapisz
         </SignupModalButton>
@@ -37,16 +37,39 @@ export function Timetable() {
             height={19.69}
             className="m-auto sm:mb-2 sm:scale-150 lg:mb-4 lg:scale-[2]"
           />
-          <span>9-15 lat</span>
+          <span>Każdy wiek</span>
         </p>
         <div className="flex flex-col justify-center gap-2">
-          <p>Grupa średnio zaawansowana</p>
-          <p>Wt 16:30-18:00</p>
+          <p>Grupa początkująca</p>
+          <p>Soboty 11:00 - 12:00</p>
         </div>
         <SignupModalButton
           color="orange"
           type="group"
-          group="9-15 lat, grupa średnio zaawansowana, wt 16:30-18:00"
+          group="Grupa początkująca, soboty 11:00 - 12:00"
+        >
+          Zapisz
+        </SignupModalButton>
+      </div>
+      <div className="m-auto grid w-full max-w-[700px] grid-cols-[max-content_1fr_min-content] items-center gap-4 rounded-[50px] bg-[#262626] px-4 py-3 sm:gap-6 sm:px-8 sm:py-6">
+        <p className="flex max-w-36 flex-col items-center text-center text-[#FBD24D]">
+          <Image
+            alt=""
+            src="/home/icons/people-yellow.svg"
+            width={24}
+            height={19.69}
+            className="m-auto sm:mb-2 sm:scale-150 lg:mb-4 lg:scale-[2]"
+          />
+          <span>Każdy wiek</span>
+        </p>
+        <div className="flex flex-col justify-center gap-2">
+          <p>Grupa średnio zaawansowana</p>
+          <p>Soboty 12:00 - 13:00</p>
+        </div>
+        <SignupModalButton
+          color="orange"
+          type="group"
+          group="Grupa średnio zaawansowana, soboty 12:00 - 13:00"
         >
           Zapisz
         </SignupModalButton>
