58edd2aa54692c433cdea307966820ddc94bcb82
Jedrzej Ratajczak <jratajczak@splashbrands.com>
2025-10-25 01:21:59 +0200

    Remove unused admin pages

--- src/components/admin/index.ts
+++
@@ -1,5 +0,0 @@
-export { ButtonProduct } from './ButtonProduct';
-export { Form, type FormValues } from './Form';
-export { FormTitle } from './FormTitle';
-export { TableBody } from './TableBody';
-export { TableHead } from './TableHead';
